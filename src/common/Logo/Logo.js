21aa6c3ce4fd8dea932f7d64e398395bf41a4744
dim <jimstef@outlook.com>
2020-12-03 00:03:00 +0200

    fixed wrong css style

--- src/common/Logo/Logo.js
+++ src/common/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo(){
   return (
     <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="logo"
-    style={{height:40,width:50, objectFit:'cover', borderRadius:'100%'}}/>
+    style={{height:50,width:50, objectFit:'cover', borderRadius:'100%'}}/>
   )
 }
 
